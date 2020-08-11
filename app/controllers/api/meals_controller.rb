f692ca54108085d1c294e73c1828c25417a09ae9
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2020-08-10 21:38:49 -0400

    make join model on submit

--- app/controllers/api/meals_controller.rb
+++ app/controllers/api/meals_controller.rb
@@ -8,6 +8,8 @@ class Api::MealsController < ApplicationController
     def create
         meal = Meal.new(meal_params)
         if meal.save
+            ingr = Ingredient.find_by_name(params["ingredient"])
+            join = MealsIngredient.create(ingredient_id: ingr.id, meal_id: meal.id)
             render json: MealSerializer.new(meal), status: :accepted
         else
             render json: {errors: meal.errors.full_messages}, status: :unprocessable_entity
